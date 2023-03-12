773952ca00c4b3b2a788f2f59b483b34702a77f2
Kakadiya-Harshal <113292495+Kakadiya-Harshal@users.noreply.github.com>
2023-03-12 19:50:25 +0530

    updated aboutUsScreen

--- frontend/src/screens/AboutUsScreen.js
+++ frontend/src/screens/AboutUsScreen.js
@@ -9,14 +9,8 @@ const AboutUsScreen = () => {
         <Col md={6} className='aboutpage'>
           <h1>Who are We?</h1>
           <p>
-            Actually We are not any professionals. This website has been created
-            for the purpose of aiding students for buying and selling the notes
-            and other items that they no longer need which might be useful for
-            other new students. We take no charge for this. What we do is just
-            aid in establishing the communication between buyer and seller.These
-            can include: notes, drawing instruments, utensils & furnitures
-            specially by students who are on the verge of leaving campus sooner
-            or later.
+            We are third-year IT students at the DDIT University.
+            Indeed, we are not professionals, but we created this website to allow anyone to sell a used book that could help someone else.
           </p>
         </Col>
         <Col md={3}></Col>
@@ -26,24 +20,24 @@ const AboutUsScreen = () => {
         <Col md={6} className='aboutpage1'>
           <h1>Developer</h1>
           <p>
-            Designed and developed by{' '}
-            <a href='https://upendradhamala.com.np' target='_blank'>
-              Upendra Dhamala
-            </a>{' '}
+            Designed and developed by Kakadiya Harshal and Jay Kantariya
+            {/* <a href='https://upendradhamala.com.np' target='_blank'> */}
+
+            {/* </a>{' '} */}
           </p>
-          <h3>Who am I?</h3>
+          {/* <h3>Who am I?</h3>
           <p>
             I am Upendra Dhamala. I am a native resident of Achham, Nepal. I am
             currently studying Computer Engineering(final year) at IOE, WRC
             Pokhara. There might be some bugs in this website as I am not any
             professional and currently a web developer in making.
-          </p>
+          </p> */}
           <h3>Contact Details</h3>
-          <i className='fas fa-phone'></i> 9864421289
+          <i className='fas fa-phone'></i> 9909335856
           <br />
           <i className='fas fa-envelope-square'></i>{' '}
-          <a target='_blank' href={`mailto:dhamalaupendra@gmail.com`}>
-            dhamalaupendra@gmail.com
+          <a target='_blank' href={`mailto:patelharshal@gmail.com`}>
+            patelharshal@gmail.com
           </a>
           <br />
         </Col>
