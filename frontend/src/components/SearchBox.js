f433d3b15e3b04cb8779c415df2d9fce7c3db3ec
jaykantariya123 <113932440+jaykantariya123@users.noreply.github.com>
2023-05-26 11:24:08 +0530

    voice search added

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,16 +1,41 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState('')
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
   const submitHandler = (e) => {
+
     e.preventDefault()
     if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else if (transcript.trim()) {
+
+      setKeyword(transcript)
       history.push(`/search/${keyword}`)
     } else {
       history.push('/')
     }
+
   }
+
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  const startListening = () => {
+    SpeechRecognition.startListening();
+
+  };
   return (
     <Form onSubmit={submitHandler} inline>
       <Form.Control
@@ -19,7 +44,16 @@ const SearchBox = ({ history }) => {
         onChange={(e) => setKeyword(e.target.value)}
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'
+        id='title'
+        // value={transcript}
+        value={keyword}
       ></Form.Control>
+      <div>
+        {/* <p>Microphone: {listening ? 'on' : 'off'}</p> */}
+        <button onClick={startListening} ><i class="fa fa-microphone" aria-hidden="true"></i></button>
+        <p>{transcript}</p>
+
+      </div>
       <Button type='submit' variant='outline-success' className='p-2'>
         Search
       </Button>
